1d31478fb91b101634fbfe8c4a721627aa510963
obr-note <k33mdukb9n@yahoo.co.jp>
2020-04-19 13:19:13 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREAMENT'
+export const DECREMENT = 'DECREAMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
+})
